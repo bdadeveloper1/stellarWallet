7a0368151bedc3940bad4cf386e230ec99720d04
Christopher Rutherford <chris36787@gmail.com>
2021-07-11 15:43:19 -0400

    pages for important existing wallet

--- application.py
+++ application.py
@@ -1,43 +1,56 @@
 from flask import Flask, render_template, request
-from stellar_sdk import Server, Keypair
+from stellar_sdk import Server, Keypair, TransactionBuilder, Network
 import requests
 
 application = Flask(__name__)
 server = Server(horizon_url="https://horizon-testnet.stellar.org")
 base_fee = server.fetch_base_fee()
 
-# sample address for checking balance, sending XLM
+# sample address for checking balance and sending XLM
 # GBQMBYUUN2I6HLQ3OPDQRYI7AK5WWAXC6TDIG4UNWR7HHRPRJCHB6YBG
 
 @application.route('/')
 def home():
+    """homepage"""
     return render_template("main.html")
 
 @application.route('/create')
 def create():
+    """page for explaining seed phrase before creating wallet"""
     return render_template("create.html")
 
 @application.route('/create_phrase')
 def create_phrase():
+    """page for displaying seed phrase"""
     new_keypair = new_wallet()
     phrase = new_keypair.generate_mnemonic_phrase()
     return render_template("create_phrase.html", phrase = phrase,
     public_key = new_keypair.public_key)
 
+# to do: save wallet once created
 def new_wallet():
+    """"Function to create a new wallet"""
     keypair = Keypair.random()
     return keypair
-    
+
+# to do: save the user's wallet once it is imported
+@application.route("/import_wallet")
+def import_wallet():
+    """page for importing a new wallet using 12 word seed phrase"""
+    return render_template("import_wallet.html")
+
+@application.route('/imported')
+def imported():
+    """page for status after entering seed phrase"""
+    return render_template("imported.html")
+
 @application.route("/check_balance", methods = ['POST', 'GET'])
 def check_balance():
+    """page asks user to input an address"""
     return render_template("check_balance.html")
-    
-@application.route("/balance", methods = ['POST', 'GET'])
-def balance():
-    balance = get_bal(request.form['address'])
-    return render_template("balance.html", balance=balance)
 
 def get_bal(address):
+    """function to retrieve wallet ballance from horizon"""
     account_url_testnet = "https://horizon-testnet.stellar.org/accounts/"+str(address)
     account_url_mainnet = "https://horizon.stellar.org/accounts/"+str(address)
     try:
@@ -47,28 +60,62 @@ def get_bal(address):
 
     balance = account_info['balances'][0]['balance']
     return balance
-    
+
+@application.route("/balance", methods = ['POST', 'GET'])
+def balance():
+    """page displays balance of previously input address"""
+    balance = get_bal(request.form['address'])
+    return render_template("balance.html", balance=balance)
+
+
 @application.route("/send", methods = ['POST', 'GET'])
-def send_money():
+def send():
+    """page for inputting data to send money"""
     return render_template("send.html")
 
 def transact():
-    pass
+    """function to actually process and confirm transactions"""
+    recipient_address = request.form['recipient_address']
+    amount = request.form['amount']
+    #ideally the user should not have to enter their private key
+    #but there is currently not an implented way to store it (cookies? etc.)
+    #at least i don't know the proper way to store a private key
+    priv_key = request.form['priv_key']
+    memo = request.form['memo']
+    transaction = (
+    TransactionBuilder(
+        source_account = server.load_account(), #somehow load in the user's PUBLIC key here
+        network_passphrase = Network.TESTNET_NETWORK_PASSPHRASE,
+        base_fee = base_fee,
+        )
+    .add_text_memo(memo)
+    .append_payment_op(recipient_address, amount)
+    .set_timeout(60) #transaction times out if unsuccessful after 1 minute
+    .build()
+    )
+    transaction.sign(priv_key) #required to verify transaction
+    response = server.submit_transaction(transaction)
+    if response['successful']:
+        transaction_info_url = "https://testnet.steexp.com/tx/"+response['hash']
+        return transaction_info_url
+    else:
+        pass
 
 @application.route("/send_conf", methods = ['POST', 'GET'])
 def send_conf():
-    return render_template("send_conf.html"#, address=address, amount=amount
-    )
+    """page to output confirmation with recipient address and amount.
+    displays link to view transaction info on stellar explorer"""
+    transact()
+    return render_template("send_conf.html",
+    address = request.form['recipient_address'],
+    amount = request.form['amount'])
+
 
 @application.route("/about")
 def about():
+    """page for info about stellar/the wallet"""
     return render_template("about.html")
 
 # run the app
 if __name__ == "__main__":
-    # Setting debug to True enables debug output. This line should be
-    # removed before deploying a production app.
- #   application.debug = True
-    application.run(
-        #ssl_context='adhoc'
-        )
+    application.run()
